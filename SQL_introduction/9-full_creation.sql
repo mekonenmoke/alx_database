18bf2018d61edf0e3785b03b6425ccb2ee3c329b
mekonen <mekoninmoke@gmail.com>
2023-08-15 12:13:15 +0300

    Create table and insert multiple value in it

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,6 @@
 -- Create table in named 'second_table'
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT) -- Insert values into table
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+-- Insert values into table
 INSERT INTO second_table
 VALUES(1, 'John', 10);
 INSERT INTO second_table
