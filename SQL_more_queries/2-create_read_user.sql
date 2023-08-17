08d25d4e51a2144f0823c14e3ac4d1a09e916eb1
mekonen <mekoninmoke@gmail.com>
2023-08-17 08:15:36 +0300

    Create user and database

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,6 @@
 -- set password to user
 -- create database
 -- then grant specific privileges
-CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost' IDENTIFIED BY 'hbtn_0d_2';
-CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2' @'localhost';
-FLUSH PRIVILEGES;
