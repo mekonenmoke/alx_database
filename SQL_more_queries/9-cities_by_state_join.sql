2ede4ae03d8e867b5d68d8e4c25d7ce46a9d465c
mekonen <mekoninmoke@gmail.com>
2023-08-17 07:44:12 +0300

    Select with join and without join statement

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,8 @@
 -- a script that lists all the cities of California 
 -- that can be found in the database hbtn_0d_usa.
 SELECT cities.id,
-    cities.name
-FROM cities,
-    states
-WHERE cities.state_id = states.id
-    AND states.name = 'California'
+    cities.name,
+    states.name AS state_name
+FROM cities
+    JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
