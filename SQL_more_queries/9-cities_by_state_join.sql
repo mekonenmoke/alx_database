97fcade8524fe54740989ca64a6247a4b03a493f
mekonen <mekoninmoke@gmail.com>
2023-08-17 08:19:02 +0300

    Inner join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,7 +2,7 @@
 -- that can be found in the database hbtn_0d_usa.
 SELECT cities.id,
     cities.name,
-    states.name AS state_name
+    states.name
 FROM cities
-    JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+    INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
